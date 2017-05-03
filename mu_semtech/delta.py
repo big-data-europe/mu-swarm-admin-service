92dc1948a0f84ba05098701e4796ed7a06f5c5c9
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-05-03 12:06:51 +0200

    Adapted to the spec changes

--- mu_semtech/delta.py
+++ mu_semtech/delta.py
@@ -10,11 +10,11 @@ class DeltaDelete(Triple):
 class UpdateData:
     def __init__(self, data):
         assert isinstance(data['graph'], str)
-        assert isinstance(data['delta']['inserts'], list)
-        assert isinstance(data['delta']['deletes'], list)
+        assert isinstance(data['inserts'], list)
+        assert isinstance(data['deletes'], list)
         self.graph = data['graph']
-        self.inserts = list(map(DeltaInsert, data['delta']['inserts']))
-        self.deletes = list(map(DeltaDelete, data['delta']['deletes']))
+        self.inserts = list(map(DeltaInsert, data['inserts']))
+        self.deletes = list(map(DeltaDelete, data['deletes']))
 
     def __repr__(self):
         return "<UpdateData graph=%s inserts=%s deletes=%s>" % (self.graph, self.inserts, self.deletes)
