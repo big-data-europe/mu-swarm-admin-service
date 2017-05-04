big-data-europe/mu-swarm-admin-service

commit 26418187e093ff630be21e62fffd1ea6b58d25b5

Adapted to the spec changes

mu_semtech/pipeline.py
@@ -64,7 +64,7 @@ def update_pipelines(pipelines):
                     update_state(project.name, 'swarmui:Stopped')
                 else:
                     current_app.logger.exception(
-                        "Not implemented action: %s" % triple.o)
+                        "Not implemented action: %s" % triple.o.value)
 
 
 def update_repositories(repositories):

mu_semtech/delta.py
@@ -10,11 +10,11 @@ class DeltaDelete(Triple):
 class UpdateData:
     def __init__(self, data):
         assert isinstance(data['graph'], str)
-        assert isinstance(data['delta']['inserts'], list)
-        assert isinstance(data['delta']['deletes'], list)
+        assert isinstance(data['inserts'], list)
+        assert isinstance(data['deletes'], list)
         self.graph = data['graph']
-        self.inserts = list(map(DeltaInsert, data['delta']['inserts']))
-        self.deletes = list(map(DeltaDelete, data['delta']['deletes']))
+        self.inserts = list(map(DeltaInsert, data['inserts']))
+        self.deletes = list(map(DeltaDelete, data['deletes']))
 
     def __repr__(self):
         return "<UpdateData graph=%s inserts=%s deletes=%s>" % (self.graph, self.inserts, self.deletes)

mu_semtech/service.py
@@ -15,12 +15,12 @@ def update_services(services):
     for subject, triples in services.items():
         for triple in triples:
             if triple.p == swarmui.get("scaling"):
-                pipeline_iri = get_service_pipeline(triple.s)
+                pipeline_iri = get_service_pipeline(triple.s.value)
                 service_name = get_resource_title(subject)
                 project_id = get_resource_id(pipeline_iri)
                 project = open_project(project_id)
                 service = project.get_service(service_name)
-                service.scale(int(triple.o))
+                service.scale(int(triple.o.value))
 
 
 class ServiceScale(BasePipelineResource):

server.py
@@ -27,27 +27,28 @@ api.add_resource(ServiceInspect, '/services/<service_id>/inspect')
 
 @app.route("/update", methods=['POST'])
 def receive_update():
-    data = [UpdateData(x) for x in flask.request.get_json()]
+    data = [UpdateData(x) for x in flask.request.get_json()['delta']]
+    app.logger.debug("Received: %r", data)
     try:
         my_data = next(x for x in data if x.graph == graph)
     except StopIteration:
         return ("", 204)
     pipelines = {
-        x.s: list(my_data.filter_inserts(lambda y: y.s == x.s))
+        x.s.value: list(my_data.filter_inserts(lambda y: y.s == x.s))
         for x in my_data.filter_inserts(
-            lambda x: x.s.startswith("http://swarmui.semte.ch/resources/pipelines/"))
+            lambda x: x.s.value.startswith("http://swarmui.semte.ch/resources/pipelines/"))
     }
     update_pipelines(pipelines)
     services = {
-        x.s: list(my_data.filter_inserts(lambda y: y.s == x.s))
+        x.s.value: list(my_data.filter_inserts(lambda y: y.s == x.s))
         for x in my_data.filter_inserts(
-            lambda x: x.s.startswith("http://swarmui.semte.ch/resources/services/"))
+            lambda x: x.s.value.startswith("http://swarmui.semte.ch/resources/services/"))
     }
     update_services(services)
     repositories = {
-        x.s: list(my_data.filter_inserts(lambda y: y.s == x.s))
+        x.s.value: list(my_data.filter_inserts(lambda y: y.s == x.s))
         for x in my_data.filter_inserts(
-            lambda x: x.s.startswith("http://swarmui.semte.ch/resources/repositories/"))
+            lambda x: x.s.value.startswith("http://swarmui.semte.ch/resources/repositories/"))
     }
     update_repositories(repositories)
     return ("", 204)

sparql/triple.py
@@ -1,15 +1,39 @@
+class Value:
+    def __init__(self, data):
+        assert isinstance(data, dict)
+        assert "type" in data
+        assert isinstance(data['type'], str)
+        assert "value" in data
+        assert isinstance(data['value'], str)
+        self.type = data['type']
+        self.value = data['value']
+        self.datatype = data.get('datatype')
+
+    def __eq__(self, other):
+        if isinstance(other, Value):
+            return (
+                self.type is other.type and self.value is other.value and
+                self.datatype is other.datatype
+            )
+        else:
+            return self.value == other
+
+    def __repr__(self):
+        return "<%s type=%s value=%s datatype=%s>" % \
+            (self.__class__.__name__, self.type, self.value, self.datatype)
+
 class Triple:
     def __init__(self, data):
         assert isinstance(data, dict)
         assert "s" in data
-        assert isinstance(data['s'], str)
+        assert isinstance(data['s'], dict)
         assert "p" in data
-        assert isinstance(data['p'], str)
+        assert isinstance(data['p'], dict)
         assert "o" in data
-        assert isinstance(data['o'], str)
-        self.s = data['s']
-        self.p = data['p']
-        self.o = data['o']
+        assert isinstance(data['o'], dict)
+        self.s = Value(data['s'])
+        self.p = Value(data['p'])
+        self.o = Value(data['o'])
 
     def __repr__(self):
         return "<%s s=%s p=%s o=%s>" % (self.__class__.__name__, self.s, self.p, self.o)

commit 789b933cd0cfec34acf2b5ac913d0d4f75c4feff

Fixed bug introduced by 26418187e093ff630be21e62fffd1ea6b58d25b5

mu_semtech/pipeline.py
@@ -78,7 +78,7 @@ def update_repositories(repositories):
                 assert data
                 repository, = tuple(data.values())
                 location = repository[doap + 'location'][0]['value']
-                project_id = get_resource_id(triple.o)
+                project_id = get_resource_id(triple.o.value)
                 try:
                     existing_repo = git.Repo('/data/%s' % project_id)
                 except git.exc.NoSuchPathError: