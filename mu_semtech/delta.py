e5da069c8508d721c8f0665276745fdc4eb25fab
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-05-05 12:51:20 +0200

    Removes null operations from deltas

--- mu_semtech/delta.py
+++ mu_semtech/delta.py
@@ -1,20 +1,17 @@
 from sparql import Triple
 
 
-class DeltaInsert(Triple):
-    pass
-
-class DeltaDelete(Triple):
-    pass
-
 class UpdateData:
     def __init__(self, data):
         assert isinstance(data['graph'], str)
         assert isinstance(data['inserts'], list)
         assert isinstance(data['deletes'], list)
         self.graph = data['graph']
-        self.inserts = list(map(DeltaInsert, data['inserts']))
-        self.deletes = list(map(DeltaDelete, data['deletes']))
+        inserts = set(map(Triple, data['inserts']))
+        deletes = set(map(Triple, data['deletes']))
+        null_operations = inserts & deletes
+        self.inserts = list(inserts - null_operations)
+        self.deletes = list(deletes - null_operations)
 
     def __repr__(self):
         return "<UpdateData graph=%s inserts=%s deletes=%s>" % (self.graph, self.inserts, self.deletes)
