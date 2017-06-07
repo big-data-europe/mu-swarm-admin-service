d396128f0dfb40d9956b053312ec104d36cfff9f
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-06-07 15:42:28 +0200

    Added service start/stop actions

--- muswarmadmin/services.py
+++ muswarmadmin/services.py
@@ -1,6 +1,6 @@
 from aiohttp import web
 from aiosparql.escape import escape_any
-from aiosparql.syntax import Literal
+from aiosparql.syntax import IRI, Literal
 import logging
 
 from muswarmadmin.prefixes import SwarmUI
@@ -8,6 +8,26 @@ from muswarmadmin.prefixes import SwarmUI
 
 MAXIMUM_LINE_OF_LOGS = 1000
 logger = logging.getLogger(__name__)
+
+
+_state_to_action = {
+    SwarmUI.Started: (["start"], SwarmUI.Starting),
+    SwarmUI.Stopped: (["stop"], SwarmUI.Stopping),
+}
+
+
+async def do_action(app, project_id, service_id,
+                    args, pending_state, end_state):
+    logger.info("Changing service %s status to %s", service_id, end_state)
+    await app.update_state(service_id, pending_state)
+    service_name = await app.get_dct_title(service_id)
+    all_args = list(args) + [service_name]
+    proc = await app.run_command("docker-compose", *all_args,
+                                 cwd="/data/%s" % project_id)
+    if proc.returncode is not 0:
+        await app.update_state(service_id, SwarmUI.Error)
+    else:
+        await app.update_state(service_id, end_state)
 
 
 async def restart_action(app, project_id, service_id):
@@ -44,7 +64,22 @@ async def scaling_action(app, project_id, service_id, value):
 async def update(app, inserts, deletes):
     for subject, triples in inserts.items():
         for triple in triples:
-            if triple.p == SwarmUI.restartRequested:
+            if triple.p == SwarmUI.requestedStatus:
+                assert isinstance(triple.o, IRI), \
+                    "wrong type: %r" % type(triple.o)
+                service_id = await app.get_resource_id(subject)
+                project_id = await app.get_service_pipeline(service_id)
+                await app.reset_status_requested(service_id)
+                if triple.o in _state_to_action:
+                    args, pending_state = _state_to_action[triple.o]
+                    await app.enqueue_action(
+                        project_id, do_action,
+                        [app, project_id, service_id, args, pending_state,
+                         triple.o])
+                else:
+                    logger.error("Requested status not implemented: %s",
+                                 triple.o.value)
+            elif triple.p == SwarmUI.restartRequested:
                 assert isinstance(triple.o, Literal), \
                     "wrong type: %r" % type(triple.o)
                 if not triple.o == "true":
