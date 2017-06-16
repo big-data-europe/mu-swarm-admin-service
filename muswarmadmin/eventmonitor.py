edd1d438387ad49f07cfdcf53c5a5281231b6b94
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-06-16 09:17:58 +0200

    Added missing file

---
+++ muswarmadmin/eventmonitor.py
@@ -0,0 +1,19 @@
+import asyncio
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+async def event_monitor(docker, handlers):
+    try:
+        logger.debug("Event monitor started")
+        async for event in docker.events(decode=True):
+            if event["Type"] in handlers:
+                for handler in handlers[event["Type"]]:
+                    try:
+                        await handler(event)
+                    except Exception:
+                        logger.exception("Event handler %r failed", handler)
+    except asyncio.CancelledError:
+        logger.debug("Event monitor stopped")
