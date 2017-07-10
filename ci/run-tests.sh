827e744ec28d881dc949c5fcfdcde77003bc4cc7
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-07-10 16:54:18 +0200

    Fixed file path issue with codecov

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -12,4 +12,4 @@ exec docker run -it --rm \
 	-e MU_SPARQL_ENDPOINT=http://delta:8890/sparql \
 	-v $PWD:/src \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	bde2020/mu-swarm-admin-service:latest tox
+	bde2020/mu-swarm-admin-service:latest /bin/bash -c "tox && pip install codecov && codecov --token=${CODECOV_TOKEN?}"
