2af56053fcf533f37e91842c46468a3306f9cb9d
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-07-13 17:51:08 +0200

    Added unit tests for delta

--- tests/unit/helpers.py
+++ tests/unit/helpers.py
@@ -1,14 +1,30 @@
 import uuid
 from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
+from aiosparql.test_utils import TestSPARQLClient
 
 import muswarmadmin.main
+from muswarmadmin import delta
 
 __all__ = ['UnitTestCase', 'unittest_run_loop']
 
 
+class Application(muswarmadmin.main.Application):
+    @property
+    def sparql(self):
+        if not hasattr(self, '_sparql'):
+            self._sparql = TestSPARQLClient(self)
+        return self._sparql
+
+    @property
+    def docker(self):
+        raise RuntimeError("Can not do Docker queries during unit test")
+
+
 class UnitTestCase(AioHTTPTestCase):
     async def get_application(self):
-        return muswarmadmin.main.Application()
+        app = Application()
+        app.router.add_post("/update", delta.update)
+        return app
 
     def uuid4(self):
         return str(uuid.uuid4()).replace("-", "").upper()
