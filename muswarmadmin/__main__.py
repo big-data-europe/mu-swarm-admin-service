c953479e6bae5197d500b49c4fc7acc075ca94aa
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-07-17 13:43:14 +0200

    Fixed: better application cleanup & tests
    If the database is not accessible, it will fail to start.
    
    If Docker is not accessible, it will fail to start.
    
    If an error occur during the monitoring of the Docker event that is not
    an error in the event processing itself, it will quit the application.
    
    The error messages in those cases has been reduced (no traceback).
    
    If an unexpected error not related to connection error during startup
    occur, it will display a traceback and exit the application.

--- muswarmadmin/__main__.py
+++ muswarmadmin/__main__.py
@@ -4,7 +4,13 @@ from os import environ as ENV
 from muswarmadmin.main import app
 
 
+import asyncio
+loop = asyncio.get_event_loop()
 try:
-    web.run_app(app, port=(int(ENV['PORT']) if 'PORT' in ENV else None))
-except (SystemExit, KeyboardInterrupt):
-    exit(0)
+    web.run_app(app, port=(int(ENV['PORT']) if 'PORT' in ENV else None),
+                loop=loop)
+except (SystemExit, Exception):
+    loop.run_until_complete(app.cleanup())
+    exit(1)
+finally:
+    loop.close()
