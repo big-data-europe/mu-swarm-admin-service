c56d52f326399dfa8980d4f48c351244b1832683
Esteban <fr0gs@users.noreply.github.com>
2017-09-22 17:10:45 +0200

    Detection of availabilty of the MU_SPARQL_ENDPOINT to start querying it (#57)

--- muswarmadmin/__main__.py
+++ muswarmadmin/__main__.py
@@ -1,12 +1,28 @@
 from aiohttp import web
 from os import environ as ENV
-
 from muswarmadmin.main import app
+from time import sleep
 
-
+import requests
+import logging
 import asyncio
+
+logger = logging.getLogger(__name__)
 loop = asyncio.get_event_loop()
+
 try:
+    payload = {'query': 'select distinct ?c where {[] a ?c } LIMIT 1'}
+    url = ENV['MU_SPARQL_ENDPOINT']
+    getout = False
+    while not getout:
+        r = requests.get(url, params=payload)
+        if r.ok:
+            logger.info('SPARQL endpoint is ready')
+            getout = True
+        else:
+            logger.warn('SPARQL endpoint not yet ready')
+        sleep(2)
+
     web.run_app(app, port=(int(ENV['PORT']) if 'PORT' in ENV else None),
                 loop=loop)
 except (SystemExit, Exception):
