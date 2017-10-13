99bee4f682b8bbff2c29da0d0364d8d4484781f1
Esteban <esteban.sastre@tenforce.com>
2017-10-13 16:43:41 +0200

    Better healtheck isolated functionality

--- muswarmadmin/__main__.py
+++ muswarmadmin/__main__.py
@@ -1,28 +1,38 @@
+import requests
+import logging
+import asyncio
+
 from aiohttp import web
 from os import environ as ENV
 from muswarmadmin.main import app
 from time import sleep
 
-import requests
-import logging
-import asyncio
-
 logger = logging.getLogger(__name__)
 loop = asyncio.get_event_loop()
 
-try:
+
+def pollAccumulate(count=0):
+    """
+    Poll the database until it is ready to answer queries
+    """
+    if count >= int(ENV['POLL_RETRIES']):
+        return False
     payload = {'query': 'select distinct ?c where {[] a ?c } LIMIT 1'}
     url = ENV['MU_SPARQL_ENDPOINT']
-    getout = False
-    while not getout:
-        r = requests.get(url, params=payload)
-        if r.ok:
-            logger.info('SPARQL endpoint is ready')
-            getout = True
-        else:
-            logger.warn('SPARQL endpoint not yet ready')
-        sleep(2)
+    try:
+        requests.get(url, params=payload)
+    except requests.RequestException:
+        logger.warn('SPARQL endpoint not yet ready')
+        sleep(1)
+        return pollAccumulate(count+1)
+    logger.info('SPARQL endpoint is ready')
+    return True
 
+
+if not pollAccumulate():
+    exit(1)
+
+try:
     web.run_app(app, port=(int(ENV['PORT']) if 'PORT' in ENV else None),
                 loop=loop)
 except (SystemExit, Exception):
