00768a3c86cd5c49c620d1f9211e0305835ef156
Esteban <fr0gs@users.noreply.github.com>
2017-10-23 13:08:05 +0200

    Alternate polling using aiosparql (#63)
    Alternate polling using aiosparql & Removed requests from requirements.txt

--- muswarmadmin/__main__.py
+++ muswarmadmin/__main__.py
@@ -1,8 +1,10 @@
-import requests
-import logging
 import asyncio
+import logging
 
 from aiohttp import web
+from aiosparql.client import SPARQLClient
+from aiohttp.client_exceptions import ClientConnectionError
+from aiosparql.syntax import IRI
 from os import environ as ENV
 from muswarmadmin.main import app
 from time import sleep
@@ -12,25 +14,38 @@ logger = logging.getLogger(__name__)
 loop = asyncio.get_event_loop()
 
 
-def pollAccumulate(count=0):
+async def pollAccumulate(count=0, client=None):
     """
     Poll the database until it is ready to answer queries
     """
+    if client is None:
+        client = SPARQLClient(ENV['MU_SPARQL_ENDPOINT'],
+                              graph=IRI(ENV['MU_APPLICATION_GRAPH']))
     if count >= int(ENV['POLL_RETRIES']):
+        await client.close()
         return False
-    payload = {'query': 'select distinct ?c where {[] a ?c } LIMIT 1'}
-    url = ENV['MU_SPARQL_ENDPOINT']
     try:
-        requests.get(url, params=payload)
-    except requests.RequestException:
-        logger.warn('SPARQL endpoint not yet ready')
+        result = await client.query("""
+            ASK
+            FROM {{graph}}
+            WHERE {
+                ?s ?p ?o
+            }
+            """)
+        if not result:
+            logger.warn('SPARQL endpoint not yet ready')
+            sleep(randint(1, 5))
+            return pollAccumulate(count+1, client)
+        else:
+            logger.info('SPARQL endpoint is ready')
+            await client.close()
+            return True
+    except ClientConnectionError:
         sleep(randint(1, 5))
-        return pollAccumulate(count+1)
-    logger.info('SPARQL endpoint is ready')
-    return True
+        return pollAccumulate(count+1, client)
 
 
-if not pollAccumulate():
+if not loop.run_until_complete(pollAccumulate()):
     exit(1)
 
 try:
